fdf297eb74550f58764aa544652a6703f3aba31d
seyecode <abdouseye@groupeisi.com>
2022-07-16 15:33:12 +0000

    Création de modèles et migrations

--- gestion/models.py
+++ gestion/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Etudiant(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=70)
+    age = models.IntegerField()
+    ecole = models.CharField(max_length=20)
